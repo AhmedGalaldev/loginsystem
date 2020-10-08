9af07a253ddaa91125725f249014f1c14ac9be26
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-10-08 14:00:59 +0200

    handle error messages and success

--- signup.php
+++ signup.php
@@ -5,7 +5,28 @@ require 'header.php';
 <main>
 
     <section class="main-index ">
+
+
         <h2>Signup</h2>
+        <?php
+        if (isset($_GET['error'])) {
+            if ($_GET['error'] == "emptyfield") {
+                echo "<p class='error'>Fill in all fields</p>";
+            } elseif ($_GET['error'] == "invalidemailandusername") {
+                echo "<p class='error'>InValid Email and username</p>";
+            } elseif ($_GET['error'] == "invalidemail") {
+                echo "<p class='error'>InValid Email</p>";
+            } elseif ($_GET['error'] == "invalidusername") {
+                echo "<p class='error'>InValid username</p>";
+            } elseif ($_GET['error'] == "passworddontmatch") {
+                echo "<p class='error'>Password Don't match</p>";
+            } elseif ($GET['error'] == "usertoken") {
+                echo "<p class='error'>User Token</p>";
+            }
+        } elseif ($_GET['signup'] == "success") {
+            echo "<p class='success'>Register Successfully</p>";
+        }
+        ?>
         <form action="includes/signup.inc.php" method="POST">
             <input type="text" name="username" placeholder="Username" autocomplete="on">
             <input type="email" name="email" placeholder="Email" autocomplete="on">
